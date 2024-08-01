e578348e843eaf01ddf26888c83468a3fa8f5ba1
Camila Santos <camila8925@hotmail.com>
2024-08-01 20:22:39 -0300

    first commit

---
+++ cypress/e2e/access.cy.js
@@ -0,0 +1,9 @@
+describe('login', ()=>{
+
+    it('Login com sucesso', ()=>{
+        //acessar aplicação
+        cy.visit('https://phptravels.com/demo/')
+        cy.get('#address').type(Cypress.env('TESTE'))
+        
+    })
+})
